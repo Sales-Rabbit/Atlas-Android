043f489a21b7e385373ce5965de7e0fa41591d3a
Lexi Christiansen <lexi@salesrabbit.com>
2016-06-04 19:53:05 -0600

    Makes avatar circular (needs to be resized, & needs to test with clustered avatars).

--- layer-atlas/src/main/java/com/layer/atlas/AtlasAvatar.java
+++ layer-atlas/src/main/java/com/layer/atlas/AtlasAvatar.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
@@ -186,7 +189,11 @@ public class AtlasAvatar extends View {
             } else {
                 target = recyclableTargets.remove(0);
             }
-            target.setBitmap(participant.getBitmap());
+            Bitmap bitmap = participant.getBitmap();
+            if (bitmap != null) {
+                bitmap = getCircularBitmap(bitmap);
+            }
+            target.setBitmap(bitmap);
             target.setUrl(participant.getAvatarUrl());
             mImageTargets.put(added, target);
             toLoad.add(target);
@@ -382,5 +389,40 @@ public class AtlasAvatar extends View {
         public List<String> existing = new ArrayList<String>();
         public List<String> added = new ArrayList<String>();
         public List<String> removed = new ArrayList<String>();
+    }
+
+    /**
+     * Crops the bitmap into a circular shape. Taken from http://stackoverflow.com/a/15537470/2423194
+     */
+    public static Bitmap getCircularBitmap(Bitmap bitmap) {
+        Bitmap output;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            output = Bitmap.createBitmap(bitmap.getHeight(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        } else {
+            output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getWidth(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        float r = 0;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            r = bitmap.getHeight() / 2;
+        } else {
+            r = bitmap.getWidth() / 2;
+        }
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(r, r, r, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
     }
 }
